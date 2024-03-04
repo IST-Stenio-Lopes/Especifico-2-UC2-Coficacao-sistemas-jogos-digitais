a42491e968abfcd126bc2c4dfe1b83f933927ada
Rayanne Barros <rayannekmb@gmail.com>
2024-03-04 14:34:31 -0300

    add aula-5

--- scripts/ue5.js
+++ scripts/ue5.js
@@ -1,117 +1,89 @@
 var question1 = {
-    type: "RADIO",
-    title: "Qual das opções abaixo corresponde à janela que exibe mensagens de depuração, avisos e erros do jogo na Unity?",
-    description: "",
-    feedbacks: [
-      {
-        optionId: "q1-option-1",
-        feedBackId: "q1-box-feedback",
-        title: "OPS!",
-        message:
-          "A cobrança para ter acesso a versões de teste é possível e até bastante comum no mercado.",
-        type: "error",
-      },
-      {
-        optionId: "q1-option-2",
-        feedBackId: "q1-box-feedback",
-        title: "Não é isso",
-        message:
-          "O acesso antecipado a um jogo geralmente é concedido quando a maioria do seu conteúdo já foi integrado e testado.",
-        type: "error",
-      },
-      {
-        optionId: "q1-option-3",
-        feedBackId: "q1-box-feedback",
-        title: "Você acertou",
-        message:
-          "É muito importante respeitar a fase de testes e todo o tempo necessário para testar o jogo corretamente.",
-        type: "success",
-      },
-      {
-        optionId: "q1-option-4",
-        feedBackId: "q1-box-feedback",
-        title: "Atenção",
-        message:
-            "A versão alfa não é a primeira versão finalizada mas sim a primeira para testar a implementação dos elementos.",
-        type: "error",
-      },
-    ],
+  type: "RADIO",
+  title: "QUESTÃO 1 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "Uma árvore de decisão é um diagrama que ajuda a visualizar e mapear possíveis resultados de uma série de escolhas relacionadas.",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "OPS!",
+      message: "Na verdade, esta é uma boa definição de árvores de decisão.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Exato!",
+      message:
+        "Isso mesmo, as árvores de decisão são diagramas que auxiliam na visualização de possíveis resultados de dentre várias escolhas.",
+      type: "success",
+    },
+  ],
 };
 
 var question2 = {
-    type: "RADIO",
-    title: "Verdadeiro ou Falso",
-    description:
-      "Avalie se a afirmação a seguir é verdadeira ou falsa.",
-    feedbacks: [
-      {
-        optionId: "q2-option-1",
-        feedBackId: "q2-box-feedback",
-        title: "Cuidado",
-        message:
-          "Na verdade o principal ponto é checar se o jogo realmente funciona sem problemas e se não há nada de errado ou ofensivo com o seu conteúdo.",
-        type: "error"
-      },
-      {
-        optionId: "q2-option-2",
-        feedBackId: "q2-box-feedback",
-        title: "Parabéns",
-        message:
-          "A  diversão é importante, mas nesse caso não é o foco.",
-        type: "success"
-      },
-    ],
+  type: "RADIO",
+  title: "QUESTÃO 2 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "Todas as decisões em uma árvore de decisão devem ter apenas um resultado possível.",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "OPS!",
+      message:
+        "Em uma árvore de decisão, cada nó de decisão pode ter múltiplos resultados possíveis, dependendo das diferentes opções ou caminhos que podem ser seguidos a partir desse nó.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Exato!",
+      message:
+        "Na verdade, uma árvore pode ter vários resultados possíveis, essa é a finalidade das árvores.",
+      type: "success",
+    },
+  ],
 };
 
 var question3 = {
-    type: "COMPLETE",
-    title:
-      "Escolha a palavra que é a resposta correta para completar as sentenças a seguir:",
-    subQuestions: [
-      {
-        subQuestionId: "q3-sq1",
-        title:
-          "1. Microfone, mouse, webcam são exemplos de periférico de _________",
-        description: "",
-        feedbackEmpty: {
-          feedBackId: "q3-sq1-box-feedback",
-          title: "OPS!",
-          message: "Complete todos os campos da frase.",
-          type: "error",
-        },
-        feedBackElementsId: ["q3-sq1-box-feedback"],
-        userAnswers: [],
-        rightAnswers: [
-          {
-            id: "q3-sq1-block-1",
-            value: "Pincéis virtuais",
-            feedbacks: [
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Não é isso",
-                message:
-                  "Esse tipo serve para avaliar se o jogo está com desafios bem equilibrados, por exemplo.",
-                type: "error",
-                value: "Balanceamento"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Você acertou",
-                message: "Ao passar nos testes de compatibilidade o jogo não terá problemas para ser executado.",
-                type: "success",
-                value: "Compatibilidade"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Ops",
-                message:
-                  "Esse tipo seria usado para avaliar como os jogadores interagem com o jogo.",
-                type: "error",
-                value: "Usabilidade"
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  type: "RADIO",
+  title: "QUESTÃO 3",
+  description:
+    "Qual das seguintes afirmações descreve melhor a principal característica das árvores de decisão utilizadas em jogos?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "A essência das árvores de decisão é precisamente a ramificação com base em diferentes condições ou decisões.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Exato!",
+      message:
+        "As árvores de decisão permitem modelar uma variedade de comportamentos para NPCs, tomando decisões com base em várias condições ou contextos.",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Enquanto árvores de decisão podem ser usadas em outras áreas para visualizar dados, em jogos, elas desempenham um papel crucial no design de comportamentos e decisões de NPCs.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Árvores de decisão, por natureza, têm múltiplas ramificações e não são lineares. Dependendo da implementação, pode ser possível reavaliar e retornar a decisões anteriores.",
+      type: "error",
+    },
+  ],
 };
