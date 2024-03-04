f9dcae7c3b0ff4da01a1fbd0576acb869a431ade
Rayanne Barros <rayannekmb@gmail.com>
2024-03-04 08:37:33 -0300

    add images and questions aula-4

--- scripts/ue4.js
+++ scripts/ue4.js
@@ -38,3 +38,52 @@ var question1 = {
     },
   ],
 };
+
+var question2 = {
+  type: "RADIO",
+  title: "QUESTÃO 2 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "O Design Pattern Adapter é um padrão de design estrutural que permite que a interface de uma classe existente seja usada como outra interface, mas requer que o código-fonte da classe existente seja modificado.",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "OPS!",
+      message:
+        "O Design Pattern Adapter permite que a interface de uma classe existente seja usada como outra interface, sem precisar modificar o código-fonte da classe existente.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Exato!",
+      message:
+        "Design Pattern Adapter é um padrão de design estrutural que permite que a interface de uma classe existente seja usada como outra interface, permitindo que classes existentes trabalhem com outras sem modificar seu código-fonte.",
+      type: "success",
+    },
+  ],
+};
+
+var question3 = {
+  type: "RADIO",
+  title: "QUESTÃO 3 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "O Padrão de Projetos Facade é um padrão de design estrutural que fornece uma interface simples para um sistema complexo, ocultando suas complexidades e fornecendo ao cliente uma maneira mais fácil de acessar o sistema.",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message: "Leia novamente sobre o padrão de projetos Facade.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Exato!",
+      message:
+        "O Padrão de Projetos Facade é como uma capa que esconde a parte complicada de um programa de computador e mostra ao usuário apenas o que é fácil de entender e usar.",
+      type: "success",
+    },
+  ],
+};
