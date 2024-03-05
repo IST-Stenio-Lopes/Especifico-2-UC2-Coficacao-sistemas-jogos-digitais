65c0978abdff8871cde726ae9a7c37fad75ca064
Rayanne Barros <rayannekmb@gmail.com>
2024-03-05 13:33:14 -0300

    add aula-6

--- scripts/ue6.js
+++ scripts/ue6.js
@@ -1,117 +1,137 @@
 var question1 = {
-    type: "RADIO",
-    title: "Qual das opções abaixo corresponde à janela que exibe mensagens de depuração, avisos e erros do jogo na Unity?",
-    description: "",
-    feedbacks: [
-      {
-        optionId: "q1-option-1",
-        feedBackId: "q1-box-feedback",
-        title: "OPS!",
-        message:
-          "A fase de lançamento é quando o jogo é oficialmente disponibilizado para o público.",
-        type: "error",
-      },
-      {
-        optionId: "q1-option-2",
-        feedBackId: "q1-box-feedback",
-        title: "Não é isso",
-        message:
-          "A fase de marketing é quando a estratégia de marketing é definida para promover o jogo antes do lançamento.",
-        type: "error",
-      },
-      {
-        optionId: "q1-option-3",
-        feedBackId: "q1-box-feedback",
-        title: "Você acertou",
-        message:
-          "Nessa fase deve-se garantir que o jogo esteja livre de bugs e que a experiência do usuário seja satisfatória.",
-        type: "success",
-      },
-      {
-        optionId: "q1-option-4",
-        feedBackId: "q1-box-feedback",
-        title: "Atenção",
-        message:
-            "A fase de pós-lançamento é quando o jogo é monitorado e avaliado quanto ao seu desempenho depois que lançado.",
-        type: "error",
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 1 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "Um grafo é composto por vértices (ou nós) e arestas (ou links).",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "OPS!",
+      message: "Na verdade, vértices e arestas são os componentes de um grafo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Exato!",
+      message:
+        "Correto! Um grafo é composto por vértices, também conhecidos como nós, e arestas, também conhecidos como links.",
+      type: "success",
+    },
+  ],
 };
 
 var question2 = {
-    type: "RADIO",
-    title: "Verdadeiro ou Falso",
-    description:
-      "Avalie se a afirmação a seguir é verdadeira ou falsa.",
-    feedbacks: [
-      {
-        optionId: "q2-option-1",
-        feedBackId: "q2-box-feedback",
-        title: "Ops",
-        message:
-          "Os jogos para consoles geralmente têm requisitos de certificação mais rigorosos.",
-        type: "error"
-      },
-      {
-        optionId: "q2-option-2",
-        feedBackId: "q2-box-feedback",
-        title: "Parabéns",
-        message:
-          "Mostra que está atento sobre as plataformas que apresentamos.",
-        type: "success"
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 2 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "Grafos são usados em jogos digitais para representar mapas de níveis, caminhos de IA, redes de diálogo, sistemas de missão e muito mais.",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "OPS!",
+      message:
+        "Você já deve ter visto jogos como, Super Mario Bros, aquele mapa clássico, é um exemplo dentre vários da aplicação de grafos em Jogos.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Exato!",
+      message:
+        "Os grafos são uma ferramenta versátil usada em muitos aspectos do desenvolvimento de jogos",
+      type: "success",
+    },
+  ],
 };
 
 var question3 = {
-    type: "COMPLETE",
-    title:
-      "Escolha a palavra que é a resposta correta para completar as sentenças a seguir:",
-    subQuestions: [
-      {
-        subQuestionId: "q3-sq1",
-        title:
-          "1. Microfone, mouse, webcam são exemplos de periférico de _________",
-        description: "",
-        feedbackEmpty: {
-          feedBackId: "q3-sq1-box-feedback",
-          title: "OPS!",
-          message: "Complete todos os campos da frase.",
-          type: "error",
-        },
-        feedBackElementsId: ["q3-sq1-box-feedback"],
-        userAnswers: [],
-        rightAnswers: [
-          {
-            id: "q3-sq1-block-1",
-            value: "Pincéis virtuais",
-            feedbacks: [
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Ops",
-                message:
-                  "Embora o método de publicação seja um aspecto importante na publicação de um jogo digital, ele não é o elemento central que garante o sucesso do jogo.",
-                type: "error",
-                value: "O método de publicação"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Você acertou",
-                message: "Um bom planejamento entre outros aspectos são fundamentais para o sucesso do jogo.",
-                type: "success",
-                value: "O planejamento"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Não é isso",
-                message:
-                  "A escolha da plataforma também é importante na publicação de um jogo digital, ela não é o elemento central que garante o sucesso do jogo.",
-                type: "error",
-                value: "A escolha da plataforma"
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 3",
+  description:
+    "Qual é a função dos nós em um grafo usado para representar o espaço em um jogo?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Os obstáculos e desafios do jogo não estão diretamente relacionados aos nós do grafo.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Exato!",
+      message: "Os nós em um grafo representam os pontos no espaço do jogo",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Os limites geográficos do mundo do jogo são definidos por outros elementos, como colisões, barreiras ou limites de terreno.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Os elementos interativos e objetos no jogo são representados de outras maneiras, como sprites, modelos 3D ou outros componentes específicos do jogo.",
+      type: "error",
+    },
+  ],
+};
+
+var question4 = {
+  type: "RADIO",
+  title: "Questão 4 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description: "Em um grafo direcionado, as arestas não têm direção.",
+  feedbacks: [
+    {
+      optionId: "q4-option-1",
+      feedBackId: "q4-box-feedback",
+      title: "Exato!",
+      message:
+        "Como o próprio nome sugere, em um grafo direcionado, cada aresta tem uma direção.",
+      type: "success",
+    },
+    {
+      optionId: "q4-option-2",
+      feedBackId: "q4-box-feedback",
+      title: "OPS!",
+      message:
+        "Na verdade, em um grafo direcionado, cada aresta tem uma direção.",
+      type: "error",
+    },
+  ],
+};
+
+var question5 = {
+  type: "RADIO",
+  title: "Questão 5 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description:
+    "O algoritmo A* é usado para encontrar o caminho mais longo em um mapa.",
+  feedbacks: [
+    {
+      optionId: "q5-option-1",
+      feedBackId: "q5-box-feedback",
+      title: "Exato!",
+      message:
+        "Como se trata de um algoritmo de otimização, o A* é usado para encontrar o caminho mais curto, não o mais longo.",
+      type: "success",
+    },
+    {
+      optionId: "q5-option-2",
+      feedBackId: "q5-box-feedback",
+      title: "OPS!",
+      message:
+        "O algoritmo A* é usado para encontrar o caminho mais curto, não o mais longo.",
+      type: "error",
+    },
+  ],
 };
