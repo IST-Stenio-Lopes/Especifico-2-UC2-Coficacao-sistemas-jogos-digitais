36a09883b1756c34c315df6d4c91631c8b501780
Rayanne Barros <rayannekmb@gmail.com>
2024-03-06 07:38:32 -0300

    image div alert

--- scripts/ue7.js
+++ scripts/ue7.js
@@ -1,117 +1,120 @@
 var question1 = {
-    type: "RADIO",
-    title: "Verdadeiro ou Falso",
-    description:
-      "Avalie se a afirmação a seguir é verdadeira ou falsa.",
-    feedbacks: [
-      {
-        optionId: "q1-option-1",
-        feedBackId: "q1-box-feedback",
-        title: "Você acertou",
-        message:
-          "Isso mostra que prestou atenção ao assunto.",
-        type: "success"
-      },
-      {
-        optionId: "q1-option-2",
-        feedBackId: "q1-box-feedback",
-        title: "Ops",
-        message:
-          "Atenção, pois a instalação de um jogo em um console geralmente é diferente da instalação em um computador, por exemplo.",
-        type: "error"
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 1 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description: "A função Start é chamada uma vez a cada quadro.",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "OPS!",
+      message:
+        "Você está confundindo! A função Start é chamada apenas uma vez, no início da execução do jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Exato!",
+      message: "A Função Start só é executada uma vez.",
+      type: "success",
+    },
+  ],
 };
 
 var question2 = {
-    type: "RADIO",
-    title: "Qual das opções abaixo corresponde à janela que exibe mensagens de depuração, avisos e erros do jogo na Unity?",
-    description: "",
-    feedbacks: [
-      {
-        optionId: "q2-option-1",
-        feedBackId: "q2-box-feedback",
-        title: "OPS!",
-        message:
-          "Antes de configurar a monetização, é necessário realizar outros passos fundamentais, como criar uma conta de desenvolvedor.",
-        type: "error",
-      },
-      {
-        optionId: "q2-option-2",
-        feedBackId: "q2-box-feedback",
-        title: "Não é isso",
-        message:
-          "Mesmo sendo importante no processo de publicação de um jogo, criar uma página de informações sobre o jogo não é o primeiro passo.",
-        type: "error",
-      },
-      {
-        optionId: "q2-option-3",
-        feedBackId: "q2-box-feedback",
-        title: "Você acertou",
-        message:
-          "É necessário criar uma conta na plataforma para poder publicar o jogo.",
-        type: "success",
-      },
-      {
-        optionId: "q2-option-4",
-        feedBackId: "q2-box-feedback",
-        title: "Atenção",
-        message:
-            "Preparar arquivos e materiais promocionais não é o primeiro passo, pois antes é fundamental realizar outras ações.",
-        type: "error",
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 2 - Avalie se a afirmação a seguir é verdadeira ou falsa.",
+  description: "Em Unity, cada GameObject pode ter apenas um componente?",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "Exato!",
+      message: "Em Unity, um GameObject pode ter vários componentes",
+      type: "success",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "OPS!",
+      message: "Em Unity, um GameObject pode ter vários componentes",
+      type: "error",
+    },
+  ],
 };
 
 var question3 = {
-    type: "COMPLETE",
-    title:
-      "Escolha a palavra que é a resposta correta para completar as sentenças a seguir:",
-    subQuestions: [
-      {
-        subQuestionId: "q3-sq1",
-        title:
-          "1. Microfone, mouse, webcam são exemplos de periférico de _________",
-        description: "",
-        feedbackEmpty: {
-          feedBackId: "q3-sq1-box-feedback",
-          title: "OPS!",
-          message: "Complete todos os campos da frase.",
-          type: "error",
-        },
-        feedBackElementsId: ["q3-sq1-box-feedback"],
-        userAnswers: [],
-        rightAnswers: [
-          {
-            id: "q3-sq1-block-1",
-            value: "Pincéis virtuais",
-            feedbacks: [
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Atenção",
-                message:
-                  "A instalação é a etapa em que o jogador vai instalar o jogo no seu dispositivo, ou seja antes de ser executado.",
-                type: "error",
-                value: "Instalação"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Você acertou",
-                message: "Graças a ela o jogador consegue jogar o jogo sem erros, com o jogo sendo aberto e executado corretamente.",
-                type: "success",
-                value: "Integração de sistemas"
-              },
-              {
-                feedBackId: "q3-sq1-box-feedback",
-                title: "Ops",
-                message:
-                  "A Configuração refere-se às opções de configuração disponíveis na hora da publicação do jogo e não garante que ele vá funcionar sem erros.",
-                type: "error",
-                value: "Configuração"
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  type: "RADIO",
+  title: "Questão 3",
+  description: "Na engine de jogos Unity, o que é um GameObject?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message: "GameObjects não são ferramentas.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message: "GameObject na verdade agregam componentes.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "Resposta correta!",
+      message:
+        "Na engine de jogos Unity, qualquer objeto é considerado um GameObject: personagens, luzes, efeitos especiais, adereço. Logo, um conjunto de componentes juntos. ",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message: "GameObject definitivamente não é uma linguagem de programação.",
+      type: "error",
+    },
+  ],
+};
+
+var question4 = {
+  type: "RADIO",
+  title: "Questão 4",
+  description: "Na engine de jogos Unity, o que é um GameObject?",
+  feedbacks: [
+    {
+      optionId: "q4-option-1",
+      feedBackId: "q4-box-feedback",
+      title: "OPS!",
+      message: "Um GameObject pode ter vários componentes e tipos diferentes.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-2",
+      feedBackId: "q4-box-feedback",
+      title: "OPS!",
+      message:
+        "Parabéns. os componentes são módulos autônomos e reutilizáveis, e a funcionalidade de um GameObject é definida pela combinação de componentes que ele possui",
+      type: "success",
+    },
+    {
+      optionId: "q4-option-3",
+      feedBackId: "q4-box-feedback",
+      title: "Resposta correta!",
+      message:
+        "A interação entre diversos componentes representa o quão potentes são os GameObjects. ",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-4",
+      feedBackId: "q4-box-feedback",
+      title: "OPS!",
+      message:
+        "Na verdade, a funcionalidade de um GameObject é definida pela combinação de componentes que ele possui.",
+      type: "error",
+    },
+  ],
 };
