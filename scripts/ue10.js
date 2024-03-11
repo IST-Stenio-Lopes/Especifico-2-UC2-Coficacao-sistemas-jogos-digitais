081099cdb1cee3d39cf254871f8bb9300766a935
Rayanne Barros <rayannekmb@gmail.com>
2024-03-11 17:01:51 -0300

    aula-10

---
+++ scripts/ue10.js
@@ -0,0 +1,131 @@
+var question1 = {
+  type: "RADIO",
+  title: "QUESTÃO 1 - Verdadeiro ou Falso",
+  description:
+    "Firebase, DynamoDB e PostGreSQL, são exemplos de SGBDs que podem ser usados no desenvolvimento de jogos.",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "Não é isso",
+      message:
+        "Resposta errada! De fato, todos esses são exemplos de sistemas de bancos de dados que podem ser usados no desenvolvimento de jogos.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "CORRETO",
+      message:
+        "Parabéns, vejo que você está prestando atenção! Esses são bons exemplos de SGBDs usados em jogos.",
+      type: "success",
+    },
+  ],
+};
+
+var question2 = {
+  type: "RADIO",
+  title: "QUESTÃO 2 - Verdadeiro ou Falso",
+  description:
+    "O Banco de dados SQLite é o mais indicado para jogos que salvam seus dados na nuvem.",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "CORRETO",
+      message:
+        "Resposta certa! É errado dizer que o SQLite é indicado para jogos que salvam seus registros na nuvem.",
+      type: "success",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Não é isso",
+      message:
+        "Resposta errada! O SQLite é um banco compacto, ótimo para projetos menores.",
+      type: "error",
+    },
+  ],
+};
+
+var question3 = {
+  type: "RADIO",
+  title: "QUESTÃO 3",
+  description:
+    "Qual das seguintes opções é um banco de dados que pode ser usado para trabalhar com bancos de dados em um jogo?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "Você acertou",
+      message:
+        "Resposta correta! O Firebase é um conjunto de serviços de computação em nuvem de back-end e plataformas de desenvolvimento de aplicativos fornecidos pelo Google. Ele hospeda bancos de dados, serviços, autenticação e integração para uma variedade de aplicativos, incluindo Android, iOS, JavaScript, Node.js, Java, Unity, PHP e C++.",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "OPS!",
+      message:
+        "Errado. O Photoshop é uma ferramenta usada para arte 2D, edição de fotos e pequenas animações.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "Não é isso",
+      message:
+        "Resposta Errada! A Unity é uma das principais Engines de jogos da atualidade.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "Opa!",
+      message:
+        "Resposta Errada! Visual studio é uma excelente ferramenta para editar códigos.",
+      type: "error",
+    },
+  ],
+};
+
+var question4 = {
+  type: "RADIO",
+  title: "QUESTÃO 4",
+  description:
+    "Quais das opções é uma ferramenta que pode ser usada para trabalhar com bancos de dados em jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q4-option-1",
+      feedBackId: "q4-box-feedback",
+      title: "Você acertou",
+      message:
+        "Certa resposta! SQLite é um sistema de banco de dados leve e que pode ser usado em jogos digitais",
+      type: "success",
+    },
+    {
+      optionId: "q4-option-2",
+      feedBackId: "q4-box-feedback",
+      title: "OPS!",
+      message:
+        "Resposta Errada! O Illustrator é uma ferramenta famosa de criação de desenhos vetoriais.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-3",
+      feedBackId: "q4-box-feedback",
+      title: "Não é isso",
+      message:
+        "Resposta errada! JavaScript é uma linguagem de programação e não uma ferramenta de banco de dados.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-4",
+      feedBackId: "q4-box-feedback",
+      title: "Opa!",
+      message:
+        "Resposta errada! Blender é um poderoso software para criação de projetos 3D.",
+      type: "error",
+    },
+  ],
+};
