cdd5d9c49b2c7d3d5d722d7310c6e21de7426b4b
Rayanne Barros <rayannekmb@gmail.com>
2024-03-15 10:55:27 -0300

    content aula-14

---
+++ scripts/ue14.js
@@ -0,0 +1,185 @@
+var question1 = {
+  type: "RADIO",
+  title: "QUESTÃO 1",
+  description:
+    "Qual das seguintes atividades NÃO faz parte do processo de finalização no desenvolvimento de jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "Não é isso",
+      message:
+        "Ops! Os testes de qualidade fazem parte sim do processo de finalização de jogos.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Ops!",
+      message:
+        "Melhor rever o assunto! Pois a correção de bugs é sim uma parte do processo de finalização de um jogo",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-3",
+      feedBackId: "q1-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops! O balanceamento é uma parte fundamental para a entrega do jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-4",
+      feedBackId: "q1-box-feedback",
+      title: "Você acertou",
+      message:
+        "Certa resposta! Apesar de ser uma atividade importante. A criação de materiais de marketing não faz parte do processo de finalização de um jogo.",
+      type: "success",
+    },
+  ],
+};
+
+var question2 = {
+  type: "RADIO",
+  title: "QUESTÃO 2",
+  description:
+    "Qual das seguintes atividades NÃO faz parte do Suporte aos jogadores?",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "Não é isso",
+      message:
+        "Ops! Reveja o conteúdo. Atualizações e correções de bugs fazem parte do suporte aos jogadores.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Ops!",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. O Suporte técnico faz parte do Suporte aos jogadores.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-3",
+      feedBackId: "q2-box-feedback",
+      title: "Você acertou",
+      message:
+        "Certa resposta! Ofertas e promoções não fazem parte do Suporte aos jogadores.",
+      type: "success",
+    },
+    {
+      optionId: "q2-option-4",
+      feedBackId: "q2-box-feedback",
+      title: "Opa!",
+      message:
+        "Errado! O atendimento ao cliente faz parte do Suporte aos jogadores.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-5",
+      feedBackId: "q2-box-feedback",
+      title: "Shii!",
+      message:
+        "Errado! Expansões e conteúdo adicional fazem parte do suporte aos jogadores.",
+      type: "error",
+    },
+  ],
+};
+
+var question3 = {
+  type: "RADIO",
+  title: "QUESTÃO 3",
+  description:
+    "Qual é uma das principais considerações da publicação de jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "Não é isso",
+      message:
+        "Ops! Reveja o conteúdo. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Ops!",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops! Reveja o conteúdo. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "Shii!",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-5",
+      feedBackId: "q3-box-feedback",
+      title: "Você acertou",
+      message:
+        "Parabéns! Todas as opções listadas são importantes considerações para a publicação de jogos digitais.",
+      type: "success",
+    },
+  ],
+};
+
+var question4 = {
+  type: "RADIO",
+  title: "QUESTÃO 4",
+  description:
+    "Qual é uma característica distintiva do modelo de negócios Free-to-Play (F2P) em jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q4-option-1",
+      feedBackId: "q4-box-feedback",
+      title: "Não é isso",
+      message: "Errado! Esse é o modelo Pay-to-play.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-2",
+      feedBackId: "q4-box-feedback",
+      title: "Ops!",
+      message: "Errado! Esse é o modelo de jogos NFT.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-3",
+      feedBackId: "q4-box-feedback",
+      title: "Opa!",
+      message: "Errado! Esse é o modelo Pay-to-play.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-4",
+      feedBackId: "q4-box-feedback",
+      title: "Você acertou",
+      message: "Correto! Você realmente estudou!",
+      type: "success",
+    },
+  ],
+};
+
+var interableImages1 = {
+  images: [
+    "./img/ue14/Slide 1.png",
+    "./img/ue14/Slide 2.png",
+    "./img/ue14/Slide 3.png",
+  ],
+  htmlImageId: "interable1",
+  index: 0,
+};
