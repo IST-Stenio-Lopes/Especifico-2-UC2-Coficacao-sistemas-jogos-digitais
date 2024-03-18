a6187f3285b6086335a91d7ce0f7b2b2eb8ec6a9
Rayanne Barros <rayannekmb@gmail.com>
2024-03-18 15:22:05 -0300

    questions ue-16

---
+++ scripts/ue16.js
@@ -0,0 +1,124 @@
+var question1 = {
+  type: "RADIO",
+  title: "QUESTÃO 1",
+  description:
+    "Qual dos seguintes elementos NÃO faz parte da pré-produção de um jogo digital?",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "Não é isso",
+      message:
+        "Errado! Conceitos como gênero(s), mecânicas e narrativa são definidos principalmente na pré-produção de um jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Ops!",
+      message:
+        "Errado! A pesquisa e a elaboração de um painel semântico (mood board) é essencial no início de um processo criativo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-3",
+      feedBackId: "q1-box-feedback",
+      title: "Opa!",
+      message:
+        "A definição das mecânicas do jogo e a elaboração de um protótipo são ações essenciais da fase de pré-produção de um jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-4",
+      feedBackId: "q1-box-feedback",
+      title: "Você acertou",
+      message: "Resposta Correta!",
+      type: "success",
+    },
+    {
+      optionId: "q1-option-5",
+      feedBackId: "q1-box-feedback",
+      title: "Shii!",
+      message:
+        "A escalação da equipe técnica e criativa é parte fundamental da pré-produção de um jogo.",
+      type: "error",
+    },
+  ],
+};
+
+var question2 = {
+  type: "RADIO",
+  title: "QUESTÃO 2",
+  description:
+    "Qual das seguintes opções NÃO descreve um requisito funcional em um jogo digital?",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "Não é isso",
+      message: "Errado! A interface de usuário é um requisito funcional.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Ops!",
+      message: "Errado! As Mecânicas de jogo são requisitos funcionais.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-3",
+      feedBackId: "q2-box-feedback",
+      title: "Você acertou",
+      message: "Correto! A Narrativa não é um requisito funcional.",
+      type: "success",
+    },
+    {
+      optionId: "q2-option-4",
+      feedBackId: "q2-box-feedback",
+      title: "Opa!",
+      message: "Errado! Progressão e metas são requisitos funcionais.",
+      type: "error",
+    },
+  ],
+};
+
+var question3 = {
+  type: "RADIO",
+  title: "QUESTÃO 3",
+  description:
+    "Qual é a principal função do Game Design Document (GDD) em um projeto de desenvolvimento de jogos?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "Não é isso",
+      message:
+        "Errado! O GDD essencialmente descreve as mecânicas, narrativa e estética do jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Ops!",
+      message:
+        "Errado! O GDD essencialmente descreve as mecânicas, narrativa e estética do jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "Você acertou",
+      message: "Resposta correta!!!",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "Opa!",
+      message:
+        "Errado! O GDD essencialmente descreve as mecânicas, narrativa e estética do jogo.",
+      type: "error",
+    },
+  ],
+};
