fc54233b99bdc0e2892610c4ba6653900a878394
Rayanne Barros <rayannekmb@gmail.com>
2024-03-18 16:18:05 -0300

    add questions ue-15

---
+++ scripts/ue15.js
@@ -0,0 +1,167 @@
+var question1 = {
+  type: "RADIO",
+  title: "QUESTÃO 1",
+  description:
+    "Qual é a melhor forma de garantir que todas as partes relevantes de um jogo sejam testadas de forma sistemática?",
+  feedbacks: [
+    {
+      optionId: "q1-option-1",
+      feedBackId: "q1-box-feedback",
+      title: "Não é isso",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-2",
+      feedBackId: "q1-box-feedback",
+      title: "Ops!",
+      message:
+        "Ops! Reveja o conteúdo. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-3",
+      feedBackId: "q1-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops, você precisa revisar o conteúdo. Volte e tente novamente. As demais alternativas também fazem parte da publicação de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q1-option-4",
+      feedBackId: "q1-box-feedback",
+      title: "Você acertou",
+      message: "É isso mesmo! Você prestou bastante atenção.",
+      type: "success",
+    },
+  ],
+};
+
+var question2 = {
+  type: "RADIO",
+  title: "QUESTÃO 2",
+  description:
+    "Qual é o objetivo da fase Beta do desenvolvimento de um jogo digital?",
+  feedbacks: [
+    {
+      optionId: "q2-option-1",
+      feedBackId: "q2-box-feedback",
+      title: "Não é isso",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. Nesta fase o teste é realizado com Beta Testers, que fornecem feedback num estágio mais avançado de desenvolvimento.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-2",
+      feedBackId: "q2-box-feedback",
+      title: "Você acertou",
+      message: "É isso mesmo! Você prestou bastante atenção.",
+      type: "success",
+    },
+    {
+      optionId: "q2-option-3",
+      feedBackId: "q2-box-feedback",
+      title: "Ops!",
+      message:
+        "Ops! Reveja o conteúdo. Na verdade isso descreve uma demonstração de jogo, em caráter de teaser.",
+      type: "error",
+    },
+    {
+      optionId: "q2-option-4",
+      feedBackId: "q2-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops, você precisa revisar o conteúdo. Volte e tente novamente. Essa é a descrição da fase Gold do jogo.",
+      type: "error",
+    },
+  ],
+};
+
+var question3 = {
+  type: "RADIO",
+  title: "QUESTÃO 3",
+  description:
+    "Qual é uma das principais considerações da publicação de jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q3-option-1",
+      feedBackId: "q3-box-feedback",
+      title: "Não é isso",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. Gráficos e modelos 3D são assets de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-2",
+      feedBackId: "q3-box-feedback",
+      title: "Ops!",
+      message:
+        "Ops! Reveja o conteúdo. Efeitos sonoros e trilhas sonoras são assets de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-3",
+      feedBackId: "q3-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops, você precisa revisar o conteúdo. Volte e tente novamente. Texturas e materiais são assets de jogos digitais.",
+      type: "error",
+    },
+    {
+      optionId: "q3-option-4",
+      feedBackId: "q3-box-feedback",
+      title: "Você acertou",
+      message: "É isso mesmo! Você prestou bastante atenção.",
+      type: "success",
+    },
+    {
+      optionId: "q3-option-5",
+      feedBackId: "q3-box-feedback",
+      title: "Shii!",
+      message:
+        "Ops, você precisa revisar o conteúdo. Volte e tente novamente. Interfaces de usuário são assets de jogos digitais.",
+      type: "error",
+    },
+  ],
+};
+
+var question4 = {
+  type: "RADIO",
+  title: "QUESTÃO 4",
+  description:
+    "Qual das seguintes afirmações é verdadeira sobre a codificação em jogos digitais?",
+  feedbacks: [
+    {
+      optionId: "q4-option-1",
+      feedBackId: "q4-box-feedback",
+      title: "Não é isso",
+      message:
+        "Shii, vamos fazer uma revisão e tentar novamente. A codificação também abarca aspectos visuais do jogo, mesmo quando as engines automatizam aspectos dela.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-2",
+      feedBackId: "q4-box-feedback",
+      title: "Ops!",
+      message:
+        "Ops! Reveja o conteúdo. A codificação abrange todos os aspectos do jogo.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-3",
+      feedBackId: "q4-box-feedback",
+      title: "Opa!",
+      message:
+        "Ops, você precisa revisar o conteúdo. Volte e tente novamente. Trata-se de um aspecto fundamental na fase de pré-produção de um jogo, pois envolve performance, otimização, e recursos próprios.",
+      type: "error",
+    },
+    {
+      optionId: "q4-option-4",
+      feedBackId: "q4-box-feedback",
+      title: "Você acertou",
+      message: "É isso mesmo! Você prestou bastante atenção.",
+      type: "success",
+    },
+  ],
+};
